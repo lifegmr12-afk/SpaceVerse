0514300eb6a281912430fc7fba913f5ec666e11f
ddon09876567890-bit <ddon09876567890@gmail.com>
2026-08-10 17:09:47 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2448,7 +2448,7 @@ const missionsData: Mission[] = [
       'Discovered active volcanic moons (Io) and rings of Jupiter, Saturn, Uranus, and Neptune.',
       'Carrying the Golden Records, containing sounds, images, and greetings from Earth.'
     ],
-    image: 'https://space-verse-alpha.vercel.app/images/Voyager_1_2_Probes.png'
+    image: 'https://space-verse-alpha.vercel.app/images/Voyager_1_&_2_Probes.png'
   },
   {
     name: 'Gaia Star Mapping Mission',
